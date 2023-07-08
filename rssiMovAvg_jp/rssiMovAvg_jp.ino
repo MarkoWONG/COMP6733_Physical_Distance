a01dcef4ca24fa0bb3233cf29f9705ebb1e44240
John Van Pham <z3216645@unsw.edu.au>
2023-07-08 17:03:57 +1000

    Event handler: device automatically starts measuring when another one connects

--- rssiMovAvg_jp/rssiMovAvg_jp.ino
+++ rssiMovAvg_jp/rssiMovAvg_jp.ino
@@ -1,56 +1,90 @@
 #include <ArduinoBLE.h>
 
+const String commonBLEName = "contactTracing";
 
 const int samplingInterval = 250;     // RSSI sample rate in ms
 unsigned long prevMillis = 0;         // Helper for sampling rate
 
-const unsigned int sampleSize = 5;    // Size our circular buffer
-unsigned int rssiPool[sampleSize];    // Circular buffer (i.e. queue)
-unsigned int curr = 0;                // Index into circular buffer
+const unsigned int sampleSize = 5;      // Size our circular buffer
+unsigned int rssiPool[sampleSize];      // Circular buffer (i.e. queue)
+unsigned int curr;                      // Index into circular buffer
 const unsigned int rssiStarting = 100;  // Starting RSSI (intentionally large)
-unsigned int rssiMovSum = rssiStarting * sampleSize; // Moving sum of RSSI's in circular buffer
+unsigned int rssiMovSum;                 // Moving sum of RSSI's in circular buffer
 
 // Contact tracing service
-BLEService contactTracingService("b2bb2fe8-b971-4afa-988f-e350f071f0ba");
+// BLEService contactTracingService("b2bb2fe8-b971-4afa-988f-e350f071f0ba");
 
 void setup() {
-  // Set starting RSSI values
-  for (int i = 0; i < sampleSize; i++) {
-    rssiPool[i] = rssiStarting;
-  }
+  rssiValsInitialisation();
 
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);       // Using the LED as notification of close contact
 
   BLE.begin();
   BLE.setLocalName("contactTracing");
-  BLE.setAdvertisedService(contactTracingService);
-  BLE.addService(contactTracingService);
+  // BLE.setAdvertisedService(contactTracingService);
+  // BLE.addService(contactTracingService);
   BLE.advertise();
+
+  // Event handler for when peripheral is found with specific name
+  // BLE.setEventHandler(BLEDiscovered, peripheralDiscoveredWithName);
+  // BLE.scanForName(commonBLEName);
+
+  // Event handlers for when a central connects.
+  // Board acts as a peripheral when this happens.
+  BLE.setEventHandler(BLEConnected, centralConnected);
+  BLE.setEventHandler(BLEDisconnected, centralDisconnected);
+
   Serial.println("Peripheral is running");
 }
 
 void loop() {
-  BLEDevice central = BLE.central();
+  
+  // BLE.scanForName(commonBLEName);
+  // BLEDevice peripheral = BLE.available();
+  // Connect to peripheral and perform logic as a central device
 
-  if (central) {
-    Serial.print("Connection from central device: ");
-    Serial.println(central.address());
+  BLE.poll();
+}
 
-    while (central.connected()) {
-      unsigned long currMillis = millis();
-      if (currMillis - prevMillis >= samplingInterval) {
-        checkContact();
-        prevMillis = millis();
+void centralConnected(BLEDevice central) {
+  Serial.print("Event handler: Connection from central: ");
+  Serial.println(central.address());
+  while (BLE.connected()) {
+    unsigned long currMillis = millis();
+    if (currMillis - prevMillis >= samplingInterval) {
+      if (checkContact()) {
+        alertContact();
+        recordContact(central);
       }
+      prevMillis = millis();
     }
-
-    Serial.print("Disconnection from central device: ");
-    Serial.println(central.address());
   }
 }
 
-// Check if close contact has occurred, interface
+void centralDisconnected(BLEDevice central) {
+  Serial.print("Event handler: Disconnection from central: ");
+  Serial.println(central.address());
+  rssiValsInitialisation();
+}
+
+void peripheralDiscoveredWithName(BLEDevice peripheral) {
+  Serial.print("Event handler: Discovered peripheral with address: ");
+  Serial.println(peripheral.address());  
+}
+
+void rssiValsInitialisation() {
+  rssiMovSum = rssiStarting * sampleSize;
+  curr = 0;
+
+  // Set starting RSSI values
+  for (int i = 0; i < sampleSize; i++) {
+    rssiPool[i] = rssiStarting;
+  }  
+}
+
+// Check if close contact has occurred. 
+// Written this way so that we can add / change to other strategies.
 bool checkContact() {
   return checkContactRssiStrategy();
 }
@@ -60,7 +94,6 @@ bool checkContactRssiStrategy() {
   uint dist = distance(rssi);
 
   if (dist <= 30) {
-    digitalWrite(LED_BUILTIN, 1);
     return true;
   } else if (dist > 60) {
     digitalWrite(LED_BUILTIN, 0);
@@ -68,7 +101,8 @@ bool checkContactRssiStrategy() {
   return false;
 }
 
-// Compute the distance to the connected device
+// Compute the distance to the connected device.
+// TODO: need a fast calibration process for demo day.
 unsigned int distance(uint rssi) {
   uint dist;
 
@@ -118,3 +152,35 @@ unsigned int rssiMovAvg() {
 
   return rssiMovAvg;
 }
+
+// Alert method: update to preferred method (e.g. buzzer)
+void alertContact() {
+  digitalWrite(LED_BUILTIN, 1);
+}
+
+void recordContact(BLEDevice central) {
+  Serial.print("Contact with address: ");
+  Serial.println(central.address());
+}
+
+// Old void loop
+// voil loop() {
+  // BLEDevice central = BLE.central();
+
+  // if (central) {
+  //   Serial.print("Connection from central device: ");
+  //   Serial.println(central.address());
+
+  //   while (central.connected()) {
+  //     unsigned long currMillis = millis();
+  //     if (currMillis - prevMillis >= samplingInterval) {
+  //       checkContact();
+  //       prevMillis = millis();
+  //     }
+  //   }
+
+  //   Serial.print("Disconnection from central device: ");
+  //   Serial.println(central.address());
+  // }
+
+// }
