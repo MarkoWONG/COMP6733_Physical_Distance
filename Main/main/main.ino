922ec3db6839467cb633dfdacde59c9ce9800dca
John Van Pham <z3216645@unsw.edu.au>
2023-07-18 16:11:00 +1000

    Scanning for peripherals every 5ms

--- Main/main/main.ino
+++ Main/main/main.ino
@@ -1,5 +1,8 @@
 #include <ArduinoBLE.h>
 
+const int samplingInterval = 5;
+unsigned long prevMillis = 0;
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
@@ -12,48 +15,55 @@ void setup() {
   }
 
   Serial.println("Bluetooth® Low Energy Central scan");
-  BLE.advertise();
+  
 
   // start scanning for peripheral
   BLE.setLocalName("6733ContactTracing");
+  BLE.advertise();
   BLE.scan();
 
   
 }
 
 void loop() {
-  // check if a peripheral has been discovered
-  BLEDevice peripheral = BLE.available();
 
-  if (peripheral) {
-    // discovered a peripheral
-    Serial.println("Discovered a peripheral");
-    Serial.println("-----------------------");
+  unsigned long currMillis = millis();
+  if (currMillis - prevMillis >= samplingInterval) {
+    // check if a peripheral has been discovered
+    BLEDevice peripheral = BLE.available();
 
-    // print address
-    Serial.print("Address: ");
-    Serial.println(peripheral.address());
+    if (peripheral) {
+      // discovered a peripheral
+      Serial.println("Discovered a peripheral");
+      Serial.println("-----------------------");
 
-    // print the local name, if present
-    if (peripheral.hasLocalName()) {
-      Serial.print("Local Name: ");
-      Serial.println(peripheral.localName());
-    }
+      // print address
+      Serial.print("Address: ");
+      Serial.println(peripheral.address());
 
-    // print the advertised service UUIDs, if present
-    if (peripheral.hasAdvertisedServiceUuid()) {
-      Serial.print("Service UUIDs: ");
-      for (int i = 0; i < peripheral.advertisedServiceUuidCount(); i++) {
-        Serial.print(peripheral.advertisedServiceUuid(i));
-        Serial.print(" ");
+      // print the local name, if present
+      if (peripheral.hasLocalName()) {
+        Serial.print("Local Name: ");
+        Serial.println(peripheral.localName());
       }
+
+      // print the advertised service UUIDs, if present
+      if (peripheral.hasAdvertisedServiceUuid()) {
+        Serial.print("Service UUIDs: ");
+        for (int i = 0; i < peripheral.advertisedServiceUuidCount(); i++) {
+          Serial.print(peripheral.advertisedServiceUuid(i));
+          Serial.print(" ");
+        }
+        Serial.println();
+      }
+
+      // print the RSSI
+      Serial.print("RSSI: ");
+      Serial.println(peripheral.rssi());
+
       Serial.println();
     }
-
-    // print the RSSI
-    Serial.print("RSSI: ");
-    Serial.println(peripheral.rssi());
-
-    Serial.println();
+    prevMillis = millis();
   }
+  
 }
