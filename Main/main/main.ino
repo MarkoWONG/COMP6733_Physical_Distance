d9f635ebe6991aef033a6dfb9ff698b94329dc7b
John Van Pham <z3216645@unsw.edu.au>
2023-07-22 17:54:50 +1000

    Scanning protocol implemented and architecture in place

--- Main/main/main.ino
+++ Main/main/main.ino
@@ -1,10 +1,21 @@
 #include <ArduinoBLE.h>
 
-const int samplingInterval = 5;
+
+const uint samplingInterval = 1000;
+const uint sampleSize = 15;
+const uint adjustmentFactor = 1;
+const uint contactDist = 200; // cm
+const char * deviceName = "6733ContactTracing";
+
+
+// Used for looping frequency
 unsigned long prevMillis = 0;
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  
   while (!Serial);
 
   // begin initialization
@@ -15,63 +26,89 @@ void setup() {
   }
 
   Serial.println("Bluetooth® Low Energy Central scan");
-  
 
   // start scanning for peripheral
-  BLE.setLocalName("6733ContactTracing");
+  BLE.setLocalName(deviceName);
   BLE.advertise();
-  BLE.scan();
-
-  
+  // BLE.scan();
 }
 
 void loop() {
-
+  uint dist;
+  bool contact;
+  uint rssi;
   unsigned long currMillis = millis();
+
+  // Only scan for devices at this interval
   if (currMillis - prevMillis >= samplingInterval) {
-    // check if a peripheral has been discovered
+
+    BLE.scan();
+    // BLE.scanForName(deviceName);
+    // BLE.scanForAddress("c5:f2:f4:40:ed:30");
     BLEDevice peripheral = BLE.available();
+    while (peripheral) {
+      // If the device allows you to connect, take the average RSSI's
+      if (peripheral.connect()) {
+        rssi = rssiAvg(peripheral);
+        peripheral.disconnect();
 
-    if (peripheral) {
-      
-
-      // // print address
-      // Serial.print("Address: ");
-      // Serial.println(peripheral.address());
-
-      // print the local name, if present
-      if (peripheral.hasLocalName() && peripheral.localName()=="6733ContactTracing") {
-        
-        // John algo to find distance based on rssi
-        int distance = circularWait();
-
-        // Marko Stuff buzzer, etc
-
-
-        // discovered a peripheral
-        Serial.println("Discovered a peripheral");
-        Serial.println("-----------------------");
-        Serial.print("Local Name: ");
-        Serial.println(peripheral.localName());
-        // print the RSSI
-        Serial.print("RSSI: ");
-        Serial.println(peripheral.rssi());
-
-        Serial.println();
+      // If the device does not allow you to connect, then take the advertised RSSI
+      } else {
+        rssi = peripheral.rssi() * -1;
+        peripheral.disconnect();
       }
-      
 
-      // // print the advertised service UUIDs, if present
-      // if (peripheral.hasAdvertisedServiceUuid()) {
-      //   Serial.print("Service UUIDs: ");
-      //   for (int i = 0; i < peripheral.advertisedServiceUuidCount(); i++) {
-      //     Serial.print(peripheral.advertisedServiceUuid(i));
-      //     Serial.print(" ");
-      //   }
-        // Serial.println();
-      // }
+      dist = distance(rssi);
+      Serial.println("Device: " + peripheral.address() + ", RSSI: " + rssi + ", Distance: " + dist);
+
+      if (dist < contactDist) {
+        alertContact();
+        recordContact(peripheral);
+      } else {
+        // TODO: Remove this when buzzer implemented
+        digitalWrite(LED_BUILTIN, 0);
+      }
+
+      // Move onto the next device that was scanned
+      peripheral = BLE.available();
     }
+
+    Serial.println("Out of loop");
+
     prevMillis = millis();
   }
   
 }
+
+unsigned int rssiAvg(BLEDevice periph) {
+  uint currSampleSize = 0;
+  uint rssiSum = 0;
+
+  // Collect rssi samples until the required sample size is reached
+  while (currSampleSize < sampleSize) {
+    uint rssi = periph.rssi() * - 1;
+    if (rssi > 0) {
+      rssiSum += rssi;
+      currSampleSize++;
+    }
+  }
+
+  return rssiSum / currSampleSize;
+}
+
+uint distance(uint rssi) {
+  // 2PM Measurement: m = 9.909, c = 58.234
+  uint dist = max((rssi - 58.234) / 9.909, 0) * 100 * adjustmentFactor;
+  return dist;
+}
+
+void alertContact() {
+  digitalWrite(LED_BUILTIN, 1);
+}
+
+void recordContact(BLEDevice device) {
+  Serial.println("Contact with address: " + device.address());
+}
+
+
+
